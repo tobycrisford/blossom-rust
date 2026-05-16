49533f5eb932ad0921e01bbfe3025b6192dc0daa
Toby Crisford <tobycrisford@gmail.com>
2026-05-16 17:16:44 +0100

    Simplifying code, no reason for word list in each node to be optional

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ fn word_to_idx_vec(w: &String) -> Result<Vec<usize>, String> {
 
 struct WordTree<'a> {
     children: [Option<Box<WordTree<'a>>>; 26],
-    words: Option<Vec<&'a String>>,
+    words: Vec<&'a String>,
 }
 impl<'a> WordTree<'a> {
     fn add_word(&mut self, word_str: &'a String, mut word: Option<Vec<usize>>, current_idx: usize) {
@@ -56,18 +56,15 @@ impl<'a> WordTree<'a> {
             self.children[c].as_mut().unwrap().add_word(word_str, Some(unwrapped_word), current_idx + 1);
         }
         else {
-            if self.words.is_none() {
-                self.words = Some(Vec::new());
-            }
-            self.words.as_mut().unwrap().push(word_str);
+            self.words.push(word_str);
         }
     }
 
     fn find_words(&self, available_letters: &[usize], mandatory_letter: char, found_words: &mut Vec<&'a String>) {
-        if let Some(words) = &self.words {
-            let test_word = &words[0];
+        if self.words.len() > 0 {
+            let test_word = &self.words[0];
             if let Some(_) = test_word.find(mandatory_letter) {
-                found_words.extend(words.iter().copied());
+                found_words.extend(self.words.iter().copied());
             }
         }
         for letter in available_letters {
@@ -80,7 +77,7 @@ impl<'a> WordTree<'a> {
 fn build_wordtree_node<'a>() -> WordTree<'a> {
     WordTree {
         children: [const { None }; 26],
-        words: None,
+        words: Vec::new(),
     }
 }
 
